dd5735d49e2365b7cc7403d9dc8e3d38d188b263
Robson Lazaro <robsonlazaro.corp@gmail.com>
2025-08-20 10:50:54 -0300

    Função de remover implementado

--- script.js
+++ script.js
@@ -2,24 +2,30 @@ const form = document.querySelector("form")
 const inputText = document.getElementById('inputText')
 const listaAtiva = document.getElementById('lista-ativa')
 
-const divCheckbox = document.createElement("div")
-const inputCheckbox = document.createElement("input")
-const label = document.createElement("label")
-const imgDelete = document.createElement("img")
-
 form.addEventListener("submit", (event) => {
+    const divCheckbox = document.createElement("div")
+    const inputCheckbox = document.createElement("input")
+    const label = document.createElement("label")
+    const imgDelete = document.createElement("img")
+    
     event.preventDefault()
-
+    
     imgDelete.setAttribute("src", "assets/delete.svg")
+    imgDelete.classList.add("deleteIcon")
     divCheckbox.classList.add("checkbox-wrapper")
     inputCheckbox.setAttribute("type", 'checkbox')
     inputCheckbox.setAttribute("id", inputText.value)
     label.textContent = inputText.value
     label.classList.add("flex-1")
     
-    divCheckbox.appendChild(inputCheckbox)
-    divCheckbox.append(label)
-    divCheckbox.append(imgDelete)
+    divCheckbox.append(inputCheckbox, label, imgDelete)
     listaAtiva.appendChild(divCheckbox)
+})
 
+const deleteIcon = document.querySelectorAll(".deleteIcon")
+
+listaAtiva.addEventListener("click", (event) => {
+    if (event.target.classList.contains("deleteIcon")) {
+        event.target.parentElement.remove()
+    }
 })
